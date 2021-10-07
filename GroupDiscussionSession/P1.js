839b5163b0d3bd10cbcc403e3c464b1673173665
Password@github123 <pattnaikdibyajyoti883@gmail.com>
2021-10-07 23:25:11 +0530

    CSS Code Commit

---
+++ GroupDiscussionSession/P1.js
@@ -0,0 +1,19 @@
+var x = 20;
+var y = 30;
+console.log(x+y);
+let a = 5;
+let b = 7;
+console.log(5-7);
+const p = 15;
+const q = 17;
+const m = p*q;
+console.log(m);
+const c = 20;
+const d = 2;
+const n = c%d;
+console.log(n);
+const p1 = 15;
+const q1 = 17;
+const m1 = p/q;
+console.log(m1);
+
---
+++ GroupDiscussionSession/P2.js
@@ -0,0 +1,11 @@
+var x = 20;
+var y = 15;
+var z = 10;
+console.log(++x);
+console.log(++y);
+console.log(x++);
+var age = 25;
+var allowed = (age>=18)?"adult":"child";
+console.log(allowed);
+
+
